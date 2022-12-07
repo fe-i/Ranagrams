5e416e1e3ca1a2e890ff6158ebd48a5532d3ec9a
fe-i <feic47@gmail.com>
2022-12-07 12:32:59 -0500

    useIsWord checks if word is valid

--- src/hooks/useRandomWord.ts
+++ src/hooks/useRandomWord.ts
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import useIsWord from "./useIsWord";
 
 const useRandomWord = () => {
 	const [randomWord, setRandomWord] = useState("");
+	const { isWord } = useIsWord();
 	const getRandomWord = () => {
 		fetch("https://random-word-api.herokuapp.com/word?length=6")
 			.then((response) => response.json())
-			.then((data) => setRandomWord(data[0]));
+			.then(async (data) =>
+				(await isWord(data[0]))
+					? setRandomWord(data[0])
+					: getRandomWord()
+			);
 	};
 	return { randomWord, getRandomWord };
 };
