6ba25185240c477c7e3f7ec0ed543815d4c0d213
fe-i <feic47@gmail.com>
2022-12-15 17:15:36 -0500

    use random-words package instead of webapi

--- src/hooks/useRandomWord.ts
+++ src/hooks/useRandomWord.ts
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import useIsWord from "./useIsWord";
+import randomWords from "random-words";
 
 const useRandomWord = () => {
 	const [randomWord, setRandomWord] = useState("steaks".split(""));
 	const { isWord } = useIsWord();
-	const getRandomWord = () => {
-		fetch("https://random-word-api.herokuapp.com/word?length=6")
-			.then((response) => response.json())
-			.then(async (data) => {
-				(await isWord(data[0]))
-					? setRandomWord(
-							data[0].split("").sort(() => {
-								return 0.5 - Math.random();
-							})
-					  )
-					: getRandomWord();
-			});
+	const getRandomWord = async () => {
+		const word = randomWords({ maxLength: 6, join: "" });
+		(await isWord(word))
+			? setRandomWord(
+					word.split("").sort(() => {
+						return 0.5 - Math.random();
+					})
+			  )
+			: getRandomWord();
+		console.log(randomWord);
 	};
 	return { randomWord, getRandomWord };
 };
